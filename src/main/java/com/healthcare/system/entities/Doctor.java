2b5860dd6432c43f14d1e0410769fd68c0b6d7a4
Ansh Makker <amakker@griddynamics.com>
2024-03-27 12:24:41 +0530

    Patient added

---
+++ src/main/java/com/healthcare/system/entities/Doctor.java
@@ -0,0 +1,4 @@
+package com.healthcare.system.entities;
+
+public class Doctor {
+}
---
+++ src/main/java/com/healthcare/system/services/PatientService.java
@@ -0,0 +1,12 @@
+package com.healthcare.system.services;
+
+import com.healthcare.system.entities.Patient;
+import java.util.List;
+
+public interface PatientService {
+    Patient findById(Integer id);
+    List<Patient> findAll();
+    void savePatient(Patient patient);
+    void updatePatient(Patient patient);
+    void deletePatientById(Integer id);
+}
