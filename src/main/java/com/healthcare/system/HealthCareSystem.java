cb1f1bfacc53329cfeea00254f5af6c2f33064fa
Pratik Kumar <prakumar@griddynamics.com>
2024-04-28 10:58:33 +0530

    made minor changes

--- src/main/java/com/healthcare/system/HealthCareSystem.java
+++ src/main/java/com/healthcare/system/HealthCareSystem.java
@@ -2,10 +2,11 @@ package com.healthcare.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class HealthCareSystem {
     public static void main(String[] args) {
-        SpringApplication.run(HealthCareSystem.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(HealthCareSystem.class, args);
     }
 }
--- src/main/java/com/healthcare/system/configuration/DatabaseConfig.java
+++ src/main/java/com/healthcare/system/configuration/DatabaseConfig.java
@@ -1,17 +1,17 @@
 package com.healthcare.system.configuration;
 
-import com.zaxxer.hikari.HikariConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
-@Profile("dev")
-@PropertySource(value = "classpath:prod.yml", ignoreResourceNotFound = true)
+@Profile("prod")
+@PropertySource(value = "classpath:prod.properties", ignoreResourceNotFound = true)
 public class DatabaseConfig {
 
     private final Environment environment;
@@ -23,11 +23,15 @@ public class DatabaseConfig {
 
     @Bean
     public DataSource dataSource() {
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(environment.getProperty("spring.datasource.url"));
-        hikariConfig.setUsername(environment.getProperty("spring.datasource.username"));
-        hikariConfig.setUsername(environment.getProperty("spring.datasource.password"));
-        hikariConfig.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
-        return hikariConfig.getDataSource();
+        String url = environment.getProperty("spring.datasource.url");
+        String username = environment.getProperty("spring.datasource.username");
+        String password = environment.getProperty("spring.datasource.password");
+        String driverName = environment.getProperty("spring.datasource.driver-class-name");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
     }
 }
